0b7554364f12a4964d786a5be7ed1f41b99e7f43
snakehead007 <snakehead007@pm.me>
2020-12-28 13:35:07 +0100

    fixed some syntax errors

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 module.exports = {
     models : {
-        inventory = require('./models/inventory'),
-        order = require('./models/order'),
-        user = require('./models/user')
+        Inventory : require('./models/inventory'),
+        Order : require('./models/order'),
+        User : require('./models/user')
     },
-    m: {i=require('./min/m/i.min'),o=require('./min/m/o.min'),u=require('./min/m/u.min')},
     helpers:{
         apiHelper: require('./helpers/ApiHelpers'),
         bricklink: require('./helpers/bricklink'),
         logger: require('./helpers/logger')
     },
-    h:{a: require('./min/h/a.min'),b: require('./min/h/b.min'),l: require('./min/h/l.min')},
 };
