c0b30bcd3e9a0562b9ed99d1cbeb943ea83825ce
snakehead007 <snakehead007@pm.me>
2020-12-28 13:11:00 +0100

    update minified files

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
     models : {
-        inventory = require('../models/inventory'),
-        order = require('../models/order'),
-        user = require('../models/user')
+        inventory = require('./models/inventory'),
+        order = require('./models/order'),
+        user = require('./models/user')
     },
-    m: {i=require('../models/inventory'),o=require('../models/order'),u=require('../models/user')},
-
+    m: {i=require('./min/m/i.min'),o=require('./min/m/o.min'),u=require('./min/m/u.min')},
     helpers:{
-        apiHelper: require('../helpers/ApiHelpers'),
+        apiHelper: require('./helpers/ApiHelpers'),
         bricklink: require('./helpers/bricklink'),
-        logger: require('../helpers/logger')
-    }
+        logger: require('./helpers/logger')
+    },
+    h:{a: require('./min/h/a.min'),b: require('./min/h/b.min'),l: require('./min/h/l.min')},
 };
